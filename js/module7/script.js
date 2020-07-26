347890499712912725d119442437e48844e582a0
chaplyak <m.chapliak@gmail.com>
2020-07-26 18:56:00 +0300

    Task 8 Part

--- js/module7/script.js
+++ js/module7/script.js
@@ -88,23 +88,28 @@
 //     Другий параметр визначає кліькіть ячеєк в кожній строці.
 //     Третій параметр визначає елемент в який потрібно таблицю додати.
 
-let tableFun = function (cell, row, data) {
 
-
-
-}
-
-
-
-//Task7.1
-// - Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
-//     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
-// (Додатковачастина для завдання)
-
-
+// let field = document.getElementById('field')
+// let tableFun = function (row, cell, tag) {
+//     let table = document.createElement("table");
+//     for (let i = 0; i < row; i++) {
+//         let tr = document.createElement("tr");
+//         for (let j = 0; j < cell; j++) {
+//             let td = document.createElement("td");
+//             td.innerText = 'testText';
+//             tr.appendChild(td);
+//         }
+//         table.appendChild(tr);
+//     }
+// tag.appendChild(table)
+// }
+//
+// tableFun(5,6, field)
 
 //Task8
 // - Напишите «Карусель» – ленту изображений, которую можно листать влево-вправо нажатием на стрелочки.
+
+//???
 
 
 //Task9
@@ -135,30 +140,36 @@ let tableFun = function (cell, row, data) {
 //     Кинути алерт з попередженням у випадку якщо містить.
 //     Перевірку робити при натисканні на кнопку
 
-
-
-
-
-// -- создать скрипт, который берет считывает на странице (rules.html) текст и делает сбоку меню-оглавление по всем заголовкам которые есть в тексте.
-//     При клике на пункт оглавления вы должны отправляться к этому пункту в тексте
-
-
+// let wordsTwo = ['kit', 'kitchen', 'clinic', 'captivate', 'force', 'ladder', 'plastic', 'splurge', 'sail', 'nature' ]
+//
+// let fourForm = document.forms.myFourForm;
+// let exWordsTwo = fourForm.exWordsTwo;
+// let exBtnTwo = fourForm.wordsBtnTwo;
+//
+// exBtnTwo.onclick = () => {
+//     for (const word of wordsTwo) {
+//         if (exWordsTwo.value.includes(word)) {
+//             alert('Warning!')
+//         }
+//     }
+// }
+//
 
 //Task12:
 // -- взять массив пользователей
-let usersWithAddress = [
-    {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-    {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
-    {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
-    {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
-    {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
-    {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
-    {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
-    {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
-    {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
-    {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-    {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
-];
+// let usersWithAddress = [
+//     {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//     {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
+//     {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+//     {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
+//     {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+//     {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
+//     {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
+//     {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+//     {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
+//     {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//     {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
+// ];
 // Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать как вместе так и по отдельности.
 // 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
 // 2й - оставляет старше 29 лет включительно
@@ -166,24 +177,6 @@ let usersWithAddress = [
 // Данные выводить в документ
 
 
-
-let jsonUsers = JSON.stringify(usersWithAddress);
-let jsonDiv = document.createElement('div')
-jsonDiv.innerText = jsonUsers;
-document.body.appendChild(jsonDiv)
-
-let checkForms = document.forms.users;
-
-let checkStatus = checkForms.falseStatus;
-let checkAge = checkForms.age;
-let checkCity = checkForms.city;
+//?????
 
 
-
-// *****(Прям овердоз с рекурсией) Создать функцию которая принимает какой-либо элемент DOM-структуры .Функция создает в боди 2 кнопки (назад/вперед)
-// при нажатии вперед, вы переходите к дочернему элементу, при еще одном нажатии на "вперед", вы переходите к следующему дочернему элементу (лежащему на одном уровне)
-// НО если у (какого-либо)дочеренего элемента есть дети, то нажатие "вперед" позволяет нам войти внутрь элемента и  выводит первого ребенка. и тд.
-//     Когда все дети заканчиваются, мы выходим из данного дочернего элемента и переходим к следующему, лежащему с ним на одном уровне
-//
-//
-// *** При виділені сегменту тексту на сторінці він стає жирний/курсивний/або якось іншим способом змінює свій стан
