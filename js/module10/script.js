86af58c865e8edf5f17da676c688b5f5f3ba1c11
chaplyak <m.chapliak@gmail.com>
2020-08-04 00:47:32 +0300

    Module 10 done

--- js/module10/script.js
+++ js/module10/script.js
@@ -1,6 +1,6 @@
 function userCard(number) {
     let cardOptions = {
-        balance: 100,
+        balance: 100.0,
         transactionLimit: 100,
         historyLogs: [],
         key: number
@@ -46,7 +46,8 @@ function userCard(number) {
     function transferCredits(money, cardName) {
         takeCredits(money)
         if (cardOptions.transactionLimit >= money && cardOptions.balance >= money) {
-            return cardName.cardOptions.balance += money + (money * 0.05)
+            return cardName.putCredits(money - (money * (0.5/100))); // Беремо відсоток від суми переказу (можливо можна простіше?)
+
         } else {
             console.error('No Money!!')
         }
@@ -63,31 +64,63 @@ function userCard(number) {
     }
 }
 
-
-let card1 = userCard(1);
-
-card1.putCredits(5000);
-
-card1.setTransactionLimit(10000);
-
-card1.takeCredits(200);
-
-console.log(card1.getCardOptions());
-
-
 class UserAccount {
     constructor(name) {
         this.name = name;
         this.cards = [];
         this.addCards = function (nameOfCard) {
             if (this.cards.length < 3) {
-            return this.cards.push(nameOfCard)
+                for (let i = this.cards.length+1; i < this.cards.length; i++) {
+                    userCard([i])
+                }
+                return this.cards.push(nameOfCard)
             } else {
                 console.error('Ваш ліміт на кількість карт перевищенно!')
             }
         }
         this.getCardByKey = function (cardNumber) {
-            //??? як тут заретурнити карту?? \ Наскільки я поняв, проітерувати обєкт, і якщо ключ рівний значенню, то повернути його.
+           return userCard(cardNumber)
         }
     }
 }
+
+let max = new UserAccount('Max');
+
+max.addCards('Monobank')
+max.addCards('PrivatBank')
+max.addCards('OschadBank')
+
+
+
+
+console.log(max);
+
+let card1 = max.getCardByKey(1);
+let card2 = max.getCardByKey(2);
+let card3 = max.getCardByKey(3);
+
+
+card1.putCredits(5000);
+card1.setTransactionLimit(5000);
+
+console.log(card1.getCardOptions());
+console.log(card2.getCardOptions());
+console.log(card3.getCardOptions());
+console.log('__________________________')
+
+card1.transferCredits(450,card2)
+
+console.log(card1.getCardOptions());
+console.log(card2.getCardOptions());
+console.log(card3.getCardOptions());
+console.log('__________________________')
+
+card2.setTransactionLimit(500);
+card2.takeCredits(250);
+
+console.log(card1.getCardOptions());
+console.log(card2.getCardOptions());
+console.log(card3.getCardOptions());
+console.log('__________________________')
+
+
