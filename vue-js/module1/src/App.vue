aa10ff7dd7262ee5f27fc4faaf3dc002662e4724
chaplyak <m.chapliak@gmail.com>
2020-08-19 18:54:49 +0300

    vueJSs module1 Part

--- vue-js/module1/src/App.vue
+++ vue-js/module1/src/App.vue
@@ -1,12 +1,37 @@
 <template>
  <div>
+   <input
+       type="text"
+       v-model="listName"
+   >
+   <button @click="addToDo(listName)">Add</button>
 
+   <ul>
+     <li v-for="(item, index) in list" :key="index">
+       {{item}}
+       <button @click="">Delete</button>
+     </li>
+   </ul>
  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+
+  data() {
+    return {
+      list: [],
+    }
+  },
+
+  methods: {
+    addToDo(listName) {
+      this.list.push(listName);
+      console.log(this.list);
+    },
+
+  }
 
 }
 </script>
